861e2e4e3541e52d17adc5d0808085bb11170e0e
kirzo <ruben.pineda.dev@gmail.com>
2025-11-13 04:21:54 +0100

    Added KZLIB_API for other modules.

--- Source/KzLib/Public/Collision/KzGJK.h
+++ Source/KzLib/Public/Collision/KzGJK.h
@@ -10,12 +10,12 @@ struct FKzHitResult;
 namespace Kz::GJK
 {
 	/** Performs a raycast against a convex shape. */
-	bool Raycast(FKzHitResult& OutHit,
+	KZLIB_API bool Raycast(FKzHitResult& OutHit,
 							 const FVector& RayOrigin, const FVector& RayDir, float MaxDistance,
 							 const FKzShapeInstance& Shape, const FVector& ShapePosition, const FQuat& ShapeRotation);
 
 	/** Performs a GJK intersection test between two convex shapes. */
-	bool Intersect(const FKzShapeInstance& ShapeA, const FVector& PositionA, const FQuat& RotationA,
+	KZLIB_API bool Intersect(const FKzShapeInstance& ShapeA, const FVector& PositionA, const FQuat& RotationA,
 								 const FKzShapeInstance& ShapeB, const FVector& PositionB, const FQuat& RotationB,
 								 int32 MaxIterations = 20);
 }
--- Source/KzLib/Public/Collision/KzRaycast.h
+++ Source/KzLib/Public/Collision/KzRaycast.h
@@ -9,17 +9,17 @@ struct FKzHitResult;
 namespace Kz::Raycast
 {
 	// Fast path: Sphere
-	bool Sphere(FKzHitResult& OutHit, const FVector& Center, float Radius, const FVector& RayStart, const FVector& RayDir, float MaxDistance);
+	KZLIB_API bool Sphere(FKzHitResult& OutHit, const FVector& Center, float Radius, const FVector& RayStart, const FVector& RayDir, float MaxDistance);
 
 	// Fast path: Box (AABB)
-	bool Box(FKzHitResult& OutHit, const FVector& Center, const FVector& Extents, const FVector& RayStart, const FVector& RayDir, float MaxDistance);
+	KZLIB_API bool Box(FKzHitResult& OutHit, const FVector& Center, const FVector& Extents, const FVector& RayStart, const FVector& RayDir, float MaxDistance);
 
 	// Fast path: Box (OBB)
-	bool Box(FKzHitResult& OutHit, const FVector& Center, const FQuat& Rotation, const FVector& Extents, const FVector& RayStart, const FVector& RayDir, float MaxDistance);
+	KZLIB_API bool Box(FKzHitResult& OutHit, const FVector& Center, const FQuat& Rotation, const FVector& Extents, const FVector& RayStart, const FVector& RayDir, float MaxDistance);
 
 	// Fast path: Capsule
-	bool Capsule(FKzHitResult& OutHit, const FVector& Center, const FQuat& Rotation, float Radius, float HalfHeight, const FVector& RayStart, const FVector& RayDir, float MaxDistance);
+	KZLIB_API bool Capsule(FKzHitResult& OutHit, const FVector& Center, const FQuat& Rotation, float Radius, float HalfHeight, const FVector& RayStart, const FVector& RayDir, float MaxDistance);
 
 	// Fast path: Cylinder (aligned in local Z)
-	bool Cylinder(FKzHitResult& OutHit, const FVector& Center, const FQuat& Rotation, float Radius, float HalfHeight, const FVector& RayStart, const FVector& RayDir, float MaxDistance);
+	KZLIB_API bool Cylinder(FKzHitResult& OutHit, const FVector& Center, const FQuat& Rotation, float Radius, float HalfHeight, const FVector& RayStart, const FVector& RayDir, float MaxDistance);
 }
